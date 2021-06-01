94838a0af11c9a9f2049d866290b10e611a9a228
Urmil89 <60167367+urmil89@users.noreply.github.com>
2021-06-01 08:50:48 +0530

    adding imp.cs

---
+++ App_Code/IMP.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for IMP
+/// </summary>
+public class IMP
+{
+    public static String email_password = "Shiv@educamp@121";
+}
